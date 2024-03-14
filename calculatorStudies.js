3ab61af1c18a877a77c4e289e717f2cfee18844c
KauanStevanato <163459612+KauanStevanato@users.noreply.github.com>
2024-03-14 17:35:45 -0300

    Update and rename studies.js to calculatorStudies.js

---
+++ calculatorStudies.js
@@ -0,0 +1,16 @@
+function sum(firstNumber,secondNumber) {
+  return firstNumber + secondNumber
+}
+
+function subtraction(firstNumber, secondNumber) {
+  return firstNumber - secondNumber
+}
+
+function multiply(firstNumber, secondNumber){
+  return firstNumber * secondNumber
+}
+
+function division(firstNumber, secondNumber) {
+  return firstNumber / secondNumber
+}
+

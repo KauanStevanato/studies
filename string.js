9fe3a8f302574701cc726cbf4c4fd1dd594220fb
KauanStevanato <163459612+KauanStevanato@users.noreply.github.com>
2024-03-14 18:13:32 -0300

    Update string.js

--- string.js
+++ string.js
@@ -1,1 +1,20 @@
+// string e um dado de texto
 
+// Uma string é um texto , um conjunto de caracteres que você pode escrever para representar um nome, cidade, país, descrição, título de produto, etc.
+// As strings devem começar com aspas duplas "e terminar com uma.
+// Esquecer as aspas duplas nas strings é o erro mais comum que os iniciantes cometem.
+// .lengt é uma propriedade de strings que retorna o número de caracteres em uma string.
+
+let string = "isso e uma string"
+
+.length é uma propriedade em strings que retorna o número de caracteres.
+
+let stringLength = string.length
+
+// o codigo a cima retornara o numero de caracteres da palavra "String" nesse caso sera: 17
+
+
+
+function getAddressLength(address) {
+  return address.length
+}
